5ae16bb95830d0b2fccb59fe10374bc782ffc8bc
dimegliofernando <dimegliofernando@gmail.com>
2013-09-01 21:51:16 +0000

    cambio a matrices

---
+++  tp-candycrush-ui/CandyCrush/src/test/java/Tp/CandyCrush/TableroTest.java
@@ -0,0 +1,49 @@
+package Tp.CandyCrush;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import static org.mockito.Mockito.*;
+
+public class TableroTest extends TestCase {
+	private Tablero tablero;
+	private Nivel nivel;
+	
+	public void setUp(){
+		tablero = new Tablero();
+		
+		nivel = mock(Nivel.class);
+		
+		List<String> colores = new ArrayList<String>();
+		colores.add("Amarillo");
+		colores.add("Celeste");
+		colores.add("Verde");
+		colores.add("Rojo");
+		
+		when(nivel.cantidadDeCaramelos()).thenReturn(4);
+		when(nivel.caramelosDelNivel()).thenReturn(colores);// Nivel Facil
+		
+		
+		
+	}
+
+	
+	public void testIniciar(){
+		tablero.iniciar(2, 3, nivel);
+		Caramelo[][] caramelo = tablero.getCaramelos();
+		int cantidad = 0;
+		for(int x = 0; x < 2; x++){
+			for(int y = 0; y < 3; y++){
+				if(caramelo[x][y] != null){
+					cantidad = cantidad + 1;
+				}else{
+					fail();
+				}
+		
+			}
+		}
+		Assert.assertTrue(cantidad == 6);
+	}
+}
